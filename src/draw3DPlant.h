48cecd1a269a37e09027e19eec4f86d21045f902
dnzy06 <deniz.zeren.yilmaz@gmail.com>
2025-09-14 11:28:22 -0700

    header file update

--- src/draw3DPlant.h
+++ src/draw3DPlant.h
@@ -4,3 +4,17 @@
 #include <GLFW/glfw3.h>
 #include <random>
 #include <queue>
+
+#ifndef draw3DPlant_H
+#define draw3DPlant_H
+
+class draw3DPlant {
+public:
+    void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+    void drawBranch(float radius, float length, bool endBranch);
+    void createRandomQueues(std::queue<int>& randomAngles, std::queue<float>& randomLengths, float currLength, int depth);
+    void drawPlant(float radius, int depth, std::queue<int>& randomAngles, std::queue<float>& randomLengths);
+    int main();
+};
+
+#endif 
